37ced47594741dc1517bbc2af7e033143ba49da5
Aryan A <aryananumula@gmail.com>
2024-11-24 13:07:50 -0800

    commit

--- fear.py
+++ fear.py
@@ -4,24 +4,44 @@ from pygame.sprite import Sprite
 
 
 class Fear(Sprite):
-    def __init__(self, screen, type):
+    def __init__(self, screen, pos):
         Sprite.__init__(self)
+        self.pos = pos
         self.image = pg.image.load("images/player.png")
         self.rect = self.image.get_rect()
-        self.rect.center = [320, 570]
         self.screen = screen
+        self.aggro = False
+        self.lastSeen = pos
+        self.radius = 5
 
     def draw(self):
         self.screen.blit(self.image, self.rect)
 
-    def moveToPlayer(self, px, py, maze):
-        a = px - self.rect.centerx
-        b = py - self.rect.centery
-        self.rect.centerx += a / m.sqrt(a**2 + b**2) * 10
-        self.rect.centery += b / m.sqrt(a**2 + b**2) * 10
+    def agro(self, ppos):
+        if (
+            m.sqrt(
+                (self.rect.centerx - ppos[0]) ** 2 + (self.rect.centery - ppos[1]) ** 2
+            )
+            < self.radius
+        ):
+            self.aggro = True
+            self.lastSeen = ppos
+        return self.aggro
 
-    def dash(self, px, py, maze):
-        a = px - self.rect.centerx
-        b = py - self.rect.centery
+    def dist(self, ppos):
+        return m.sqrt(
+            (self.rect.centerx - ppos[0]) ** 2 + (self.rect.centery - ppos[1]) ** 2
+        )
+
+    def moveToPlayer(self, ppos):
+        a = ppos[0] - self.rect.centerx
+        b = ppos[1] - self.rect.centery
+        if self.dist(ppos) > 10:
+            self.rect.centerx += a / m.sqrt(a**2 + b**2) * 10
+            self.rect.centery += b / m.sqrt(a**2 + b**2) * 10
+
+    def dash(self, ppos):
+        a = ppos[0] - self.rect.centerx
+        b = ppos[1] - self.rect.centery
         self.rect.centerx += a / max(m.sqrt(a**2 + b**2), 1) * 50
         self.rect.centery += b / max(m.sqrt(a**2 + b**2), 1) * 50
